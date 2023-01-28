c72635b14bb92ae81a1791341e02555bc501eb4f
memsec <zbc.kun@gmail.com>
2023-01-29 01:55:12 +0300

    Fix is connected button

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -8,10 +8,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-     
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        print(sender.currentTitle as Any)
+        let hardness = sender.currentTitle 
     }
     
 }
